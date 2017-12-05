765c39c5458b89c24b7c76d517a9a6dc599ecbc7
youko-wakou <appu2pudding@gmail.com>
2017-12-05 23:49:26 +0900

    文章を元に戻す

--- app/src/main/java/techacademy/wakou/youko/timer/MainActivity.java
+++ app/src/main/java/techacademy/wakou/youko/timer/MainActivity.java
@@ -1,10 +1,9 @@
 package techacademy.wakou.youko.timer;
 
+
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 
 import java.util.Timer;
@@ -14,13 +13,11 @@ public class MainActivity extends AppCompatActivity {
 
     Timer mTimer;
     TextView mTimerText;
+
+    // タイマー用の時間のための変数
     double mTimerSec = 0.0;
 
     Handler mHandler = new Handler();
-
-    Button mStartButton;
-    Button mPauseButton;
-    Button mResetButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,53 +25,22 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mTimerText = (TextView) findViewById(R.id.timer);
-        mStartButton = (Button) findViewById(R.id.start_button);
-        mPauseButton = (Button) findViewById(R.id.pause_button);
-        mResetButton = (Button) findViewById(R.id.reset_button);
 
-        mStartButton.setOnClickListener(new View.OnClickListener() {
+        // タイマーの作成
+        mTimer = new Timer();
+        // タイマーの始動
+        mTimer.schedule(new TimerTask() {
             @Override
-            public void onClick(View v) {
-                if (mTimer == null) {
-                    mTimer = new Timer();
-                    mTimer.schedule(new TimerTask() {
-                        @Override
-                        public void run() {
-                            mTimerSec += 0.1;
+            public void run() {
+                mTimerSec += 0.1;
 
-                            mHandler.post(new Runnable() {
-                                @Override
-                                public void run() {
-                                    mTimerText.setText(String.format("%.1f", mTimerSec));
-                                }
-                            });
-                        }
-                    }, 100, 100);
-                }
+                mHandler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        mTimerText.setText(String.format("%.1f", mTimerSec));
+                    }
+                });
             }
-        });
-
-        mPauseButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (mTimer != null) {
-                    mTimer.cancel();
-                    mTimer = null;
-                }
-            }
-        });
-
-        mResetButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mTimerSec = 0.0;
-                mTimerText.setText(String.format("%.1f", mTimerSec));
-
-                if (mTimer != null) {
-                    mTimer.cancel();
-                    mTimer = null;
-                }
-            }
-        });
+        }, 100, 100);    // 最初に始動させるまで 100ミリ秒、ループの間隔を 100ミリ秒 に設定
     }
 }
